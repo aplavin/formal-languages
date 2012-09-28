51298f97770e625d8bf8d8038fcbb676d223ff4f
Alexander Plavin <me@aplavin.ru>
2012-09-28 17:43:56 +0400

    Grammar.hs changes, refactoring
    --HG--
    extra : convert_revision : 46bd4a1431061b524bec74b50d6011a0709016f4

--- src/LangDefs/Grammar.hs
+++ src/LangDefs/Grammar.hs
@@ -7,77 +7,67 @@ import Data.Maybe
 import Utils
 import Text.Groom
 
+type Production = (Char, String)
+
 data Grammar = Grammar {
  	 name :: String
- 	,start1 :: [Char]
- 	,productions :: [([Char], [Char])]
-}
+ 	,start :: Char
+ 	,productions :: [Production]
+} deriving (Show)
 
 instance LangDef Grammar where
 	_name = name
-	_accepted = accepted
+	_accepted = isAccepted
 	_representations = representations
 	_fromNameLines = fromNameLines
 
-accepted g w
-	| isCFG g = isAccepted (removeEmpty (head $ start1 g, [(from, to) | ([from], to) <- productions g])) w
-	| otherwise = error "Not context-free"
-
-isCFG g = all (\(from, to) -> length from == 1) (productions g)
-
 representations g = zip ["Haskell definition"] (map ($ g) [toHaskellDef])
 
-toHaskellDef g@(Grammar{}) = groom (removeEmpty (head $ start1 g, [(from, to) | ([from], to) <- productions g]))
+toHaskellDef = groom
 
-fromNameLines name strs = Grammar{
+fromNameLines :: String -> [String] -> Grammar
+fromNameLines name strs = if isCFG then Grammar{
 		 name=name
-		,start1=fst.head $ productions
-		,productions=productions
-	}
-	where 
-		productions = [(lhs, rhs) | s <- strs, let [lhs, rhs'] = map strip $ splitOnList "->" s, let rhs = if rhs' == "e" then "" else rhs']
+		,start=fst.head $ productions
+		,productions = removeEmpty productions
+	} else error "Not context-free grammar"
+	where
+		productions :: [Production]
+		productions = [(lhs, rhs) | s <- strs, let [[lhs], rhs'] = map strip $ splitOnList "->" s, let rhs = if rhs' == "e" then "" else rhs']
 
-class (Show s, Eq s) => Symbol s where
-	isT :: s -> Bool
-	isN :: s -> Bool
-	isT = not . isN
-	isN = not . isT
+		isCFG :: Bool
+		isCFG = all (\lhs -> length lhs == 1 && isN (head lhs)) $ map (strip.head) $ map (splitOnList "->") strs
 
-instance Symbol Char where
-	isN c = 'A' <= c && c <= 'Z'
+isN :: Char -> Bool
+isN c = 'A' <= c && c <= 'Z'
 
-type CFG s = (s, [(s, [s])])
+isT :: Char -> Bool
+isT = not . isN
 
-start :: CFG s -> s
-start = fst
+terminals :: Grammar -> [Char]
+terminals g = foldl union [] (map (filter isT.snd) $ productions g)
 
-prods :: CFG s -> [(s, [s])]
-prods = snd
+nonterminals :: Grammar -> [Char]
+nonterminals = nub . map fst . productions
 
-terminals :: Symbol s => CFG s -> [s]
-terminals g = foldl union [] (map (filter isT.snd) $ prods g)
-
-nonterminals :: Symbol s => CFG s -> [s]
-nonterminals = nub . map fst . prods
-
-symbols :: Symbol s => CFG s -> [s]
+symbols :: Grammar -> [Char]
 symbols g = terminals g `union` nonterminals g
 
-removeEmpty :: Symbol s => CFG s -> CFG s
-removeEmpty g = (start g, [(lhs, rhs') | (lhs, rhs) <- noEmptyProds, rhs' <- allReplaces rhs])
+removeEmpty :: [Production] -> [Production]
+removeEmpty prods = [(lhs, rhs') | (lhs, rhs) <- noEmptyProds, rhs' <- allReplaces rhs]
 	where
-		emptyProds = filter (null.snd) (prods g)
-		noEmptyProds = filter (not.null.snd) (prods g)
+		emptyProds = filter (null.snd) prods
+		noEmptyProds = filter (not.null.snd) prods
 
 		allReplaces prod
 			| null eProds = [prod]
 			| otherwise = prod : concat [allReplaces prod' | eProd <- eProds, let prod' = replaceOnce [eProd] [] prod]
 			where eProds = filter (`elem` prod) $ map fst emptyProds
 
-isAccepted :: Symbol s => CFG s -> [s] -> Bool
+isAccepted :: Grammar -> String -> Bool
 isAccepted g w = isAccepted' g w []
 
-isAccepted' :: Symbol s => CFG s -> [s] -> [s] -> Bool
+isAccepted' :: Grammar -> String -> String -> Bool
 isAccepted' g w stack
 	| null w = [start g] `elem` stacks'
 	| otherwise = or [isAccepted' g (tail w) st | st <- stacks']
@@ -85,8 +75,9 @@ isAccepted' g w stack
 		stack' = if null w then stack else head w : stack
 		stacks' = replaceProds stack'
 
-		prodsOnTop st = filter (\prod -> reverse (snd prod) `isPrefixOf` st) (prods g)
+		prodsOnTop st = filter (\prod -> reverse (snd prod) `isPrefixOf` st) (productions g)
 		replaceProds st
 			| null prods = [st]
 			| otherwise = st : concat [replaceProds (fst prod : drop (length $ snd prod) st) | prod <- prods]
 			where prods = prodsOnTop st
+
